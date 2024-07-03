3a4bf1b224ac6414dc999fab617e69345f2f3be5
dldonofrio <dandono97@gmail.com>
2024-07-02 21:53:52 -0700

    added button so people are less confused

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -74,6 +74,7 @@ export default function Home() {
           <br />
         </p>
         <form id="betasignupform" method="POST" onSubmit={handleSubmit}>
+          <span className="py-2 flex items-center space-x-2">
           <input 
             className="py-3 px-4 block border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600" 
             placeholder="email address" 
@@ -83,6 +84,12 @@ export default function Home() {
             onChange={(e) => setEmail(e.target.value)}
             required
           />
+          <button 
+            className="py-3 px-4 block border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
+          >
+            Notify me
+          </button>
+          </span>
         </form>
 
         <p className="text-slate-500">
